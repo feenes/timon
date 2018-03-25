ae87561424714fbe0852dbc2798ccf55a98db8ca
feenes@hydra <feenes at hydra>
2018-03-25 23:43:26 +0200

    first version ready for pypi

--- setup.py
+++ setup.py
@@ -8,21 +8,9 @@ from setuptools import setup
 
 setup(name='timon',
       version='0.0.3',
-      description='module with git hooks',
+      description='simple, low resource monitoring tool',
       classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3 :: Only',
-        'Intended Audience :: Sysadmins / DevOps',
-        'Intended Audience :: Information Technology',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Topic :: Internet',
-        'Topic :: Internet :: File Transfer Protocol (FTP)',
-        'Programming Language :: Python',
-
+            'Development Status :: 3 - Alpha',
       ],
       keywords='tiny monitor',
       url='https://www.teledomic.eu',
@@ -52,5 +40,6 @@ setup(name='timon',
       tests_require=['nose', 'pytest'],
       zip_safe=False,
       include_package_data=True,
+      python_requires='>=3.5, <4',
     )
 
